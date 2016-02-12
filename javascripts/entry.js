add64bce940434de914cc3e3df7503346eaa3bad
Nick Bucciarelli <nick.bucciarelli@gmail.com>
2016-02-12 16:12:23 -0500

    Material UI installed.

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -4,7 +4,23 @@ require('../sass/main.sass');
 
 import React from "react";
 import ReactDOM from "react-dom";
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+// // Needed for onTouchTap
+// // Can go away when react 1.0 release
+// // Check this repo:
+// // https://github.com/zilverline/react-tap-event-plugin
+// injectTapEventPlugin();
+
+import AppBar from 'material-ui/lib/app-bar';
+const AppBarExampleIcon = () => (
+  <AppBar
+    title="Title"
+    iconClassNameRight="muidocs-icon-navigation-expand-more" />
+);
 
 ReactDOM.render((
-  <div className="myDiv">Hello Electron!</div>
+  <div className="myDiv">
+    Hello Electron!
+    <AppBarExampleIcon></AppBarExampleIcon>
+  </div>
 ), document.getElementById('content'));
