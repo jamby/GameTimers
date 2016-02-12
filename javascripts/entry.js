d07073396e1780039ce1aaca805ecda51f15046f
Nick Bucciarelli <nick.bucciarelli@gmail.com>
2016-02-12 16:38:31 -0500

    Speech components.

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -1,10 +1,14 @@
 require('../sass/main.sass');
 var _  = require('underscore');
+var speech = require('./speech');
 
 'use strict';
 
 import React from "react";
 import ReactDOM from "react-dom";
+// When events are triggered, the user should know what's happening
+// import Speech from 'react-speech';
+
 // import injectTapEventPlugin from 'react-tap-event-plugin';
 // // Needed for onTouchTap
 // // Can go away when react 1.0 release
@@ -13,6 +17,26 @@ import ReactDOM from "react-dom";
 // injectTapEventPlugin();
 
 import AppBar from 'material-ui/lib/app-bar';
+//
+// class App = React.createClass({
+//   mixins: [],
+//   componentWillUnmount() {
+//
+//   },
+//   getSpeechConfig() {
+//     return [{
+//       word: 'click',
+//       action: 'handleClick',
+//       feedback: 'clicking'
+//     }];
+//   },
+//   handleClick() {
+//     console.log('clicked');
+//   },
+//   render() {
+//
+//   }
+// });
 
 const AppBarExampleIcon = () => (
   <AppBar
@@ -24,5 +48,6 @@ ReactDOM.render((
   <div className="myDiv">
     Hello Electron!
     <AppBarExampleIcon></AppBarExampleIcon>
+    {/*<Speech text="Welcome to react speech" />*/}
   </div>
 ), document.getElementById('content'));
